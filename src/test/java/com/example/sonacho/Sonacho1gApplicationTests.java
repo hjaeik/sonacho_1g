b063240c312208562161ffe92f5754436aa31bce
hjaeik <jaeik@hanmail.net>
2026-01-28 00:15:10 +0900

    init

---
+++ src/test/java/com/example/sonacho/Sonacho1gApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonacho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sonacho1gApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
